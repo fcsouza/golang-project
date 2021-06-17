ac31dc4b1e658c95dda7ade499c5a9c6aefd4d7f
fcsouza <fcsouza18@hotmail.com>
2021-06-17 19:45:52 -0300

    basic databse

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module example.com/hello
 
 go 1.16
 
-require rsc.io/quote v1.5.2
+require (
+	github.com/mattn/go-sqlite3 v1.14.7 // indirect
+	gorm.io/driver/sqlite v1.1.4 // indirect
+	gorm.io/gorm v1.21.11 // indirect
+	rsc.io/quote v1.5.2
+)
--- hello.go
+++ hello.go
@@ -1,11 +1,40 @@
 package main
 
 import (
-	"fmt"
-
-	"rsc.io/quote"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
+type Product struct {
+	gorm.Model
+	Code  string
+	Price uint
+}
+
 func main() {
-	fmt.Println(quote.Go())
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	// Migrate the schema
+	db.AutoMigrate(&Product{})
+
+	// Create
+	db.Create(&Product{Code: "D42", Price: 100})
+
+	// Read get by id
+	var product Product
+	db.First(&product, 1)
+	db.First(&product, "code = ?", "D42")
+
+	// Update
+	db.Model(&product).Update("Price", 200)
+
+	// Update - update multiple fields
+	db.Model(&product).Updates(Product{Price: 200, Code: "F42"}) // non-zero fields
+	db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
+
+	// Delete
+	db.Delete(&product, 1)
 }
