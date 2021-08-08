dda8db7e4d138e877e7e76b145de265dbdc64fd6
fcsouza <fcsouza18@hotmail.com>
2021-08-07 21:07:51 -0300

    refactor test

--- goWithTDD/hello_test.go
+++ goWithTDD/hello_test.go
@@ -27,4 +27,10 @@ func TestHello(t *testing.T) {
 		want := "Hola, Elodie"
 		assertCorrectMessage(t, got, want)
 	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Elodie", "French")
+		want := "Bonjour, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
 }
