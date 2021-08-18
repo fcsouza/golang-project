c6b48e0b38a924e82478816d5160dd675362e08f
fcsouza <fcsouza18@hotmail.com>
2021-08-18 17:09:07 -0300

    include new test

--- TDDArraysSlices/array_test.go
+++ TDDArraysSlices/array_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestSum(t *testing.T) {
 
 	t.Run("collection of 5 numbers", func(t *testing.T) {
-		numbers := [5]int{1, 2, 3, 4, 5}
+		numbers := []int{1, 2, 3, 4, 5}
 
 		got := Sum(numbers)
 		want := 15
@@ -26,4 +26,14 @@ func TestSum(t *testing.T) {
 		}
 	})
 
+}
+
+func TestSumAll(t *testing.T) {
+
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if got != want {
+		t.Errorf("got %v want %v", got, want)
+	}
 }
