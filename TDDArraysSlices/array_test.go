ce43cc337a5f7fe4f5e752089a94e13bb696d32c
fcsouza <fcsouza18@hotmail.com>
2021-08-19 17:13:51 -0300

    include sumAll test

--- TDDArraysSlices/array_test.go
+++ TDDArraysSlices/array_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
@@ -33,7 +36,7 @@ func TestSumAll(t *testing.T) {
 	got := SumAll([]int{1, 2}, []int{0, 9})
 	want := []int{3, 9}
 
-	if got != want {
+	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %v want %v", got, want)
 	}
 }
