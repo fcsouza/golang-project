9ebf82fe26402c1983272a0905b548ebf05f5579
fcsouza <fcsouza18@hotmail.com>
2021-08-25 11:32:07 -0300

    include refactor struct

--- goStructs/shapes.go
+++ goStructs/shapes.go
@@ -1,5 +1,14 @@
 package main
 
-func Perimeter(width float64, height float64) float64 {
-	return 2 * (width + height)
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Width * rectangle.Height
 }
