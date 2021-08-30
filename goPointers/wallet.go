6f506c77ba9cc83cb7bf6b111ac18d4404c43f64
fcsouza <fcsouza18@hotmail.com>
2021-08-30 13:05:22 -0300

    refator pointer code

--- goPointers/wallet.go
+++ goPointers/wallet.go
@@ -1,11 +1,13 @@
 package main
 
-type Wallet struct{}
-
-func (w Wallet) Deposit(amount int) {
-
+type Wallet struct {
+	balance int
 }
 
-func (w Wallet) Balance() int {
-	return 0
+func (w *Wallet) Deposit(amount int) {
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() int {
+	return w.balance
 }
--- goPointers/wallet_test.go
+++ goPointers/wallet_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestWallet(t *testing.T) {
 
@@ -9,6 +12,9 @@ func TestWallet(t *testing.T) {
 	wallet.Deposit(10)
 
 	got := wallet.Balance()
+
+	fmt.Printf("address of balance in test is %v \n", &wallet.balance)
+
 	want := 10
 
 	if got != want {
